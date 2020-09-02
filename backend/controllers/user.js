facec00da5367409245f4d59fa621b72e6027450
Romain <romain.sire@gmx.com>
2020-09-02 10:51:08 +0200

    add user, hash & crypt infos perso, + gestion erreur

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,26 +1,40 @@
-const mysql = require('mysql');
 require('dotenv').config();
+const bcrypt = require('bcrypt');
+const cryptojs = require('crypto-js');
 
-
+const database = require('../utils/database');
 
 
 
 exports.newuser = (req, res, next) => {
-  const connection = mysql.createConnection({
-    host     : process.env.DB_HOST,
-    user     : process.env.DB_USER,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_BASENAME,
-    charset: utf8_general_ci
-  });
+  bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+      const name = cryptojs.HmacSHA256(req.body.name, process.env.CRYPT_USER_INFO).toString();
+      const email = cryptojs.HmacSHA256(req.body.email, process.env.CRYPT_USER_INFO).toString();
+      const password = cryptojs.HmacSHA256(hash, process.env.CRYPT_USER_INFO).toString();
 
-  connection.connect();
+      const sql = "INSERT INTO Users (name, email, password, isadmin)\
+      VALUES ('" + name + "', '" + email + "', '" + password + "', 0);";
 
-  connection.query('SELECT name FROM Post_types', (error, results, fields) => {
-    if (error) throw error;
-    console.log(results[0].name);
-  });
-  
-  connection.end();
-
+      const connexion = database.connexion();
+      connexion.query(sql, (error, results, fields) => {
+        if (error) {
+          if (error.errno === 1062) {
+            // ERREUR : email déjà utilisé dans la base
+            res.status(403).json({ "error": "L'email est déjà utilisé !" });
+          } else {
+            // Autre erreur
+            res.status(500).json({ "error": error.sqlMessage });
+          }
+        } else {
+          // Pas d'erreur : utilisateur ajouté
+          res.status(201).json({
+            message: 'Utilisateur créé',
+            userId: results.insertId
+          });
+        }
+      });
+      connexion.end();
+    })
+    .catch(error => res.status(500).json({ error }));
 }
---
+++ backend/utils/database.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+
+exports.connexion = () => {
+  const connection = mysql.createConnection({
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_BASENAME,
+    charset: 'utf8_general_ci'
+  });
+
+  connection.connect();
+  
+  return connection;
+}

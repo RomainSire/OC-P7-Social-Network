e07961eb8c098f1e0c2a5b8e3a5de0c26f5514bc
Romain <romain.sire@gmx.com>
2020-09-04 15:48:13 +0200

    Refonte de Multer pour effectuer une validation sur les images envoyées

--- backend/middlewares/multer-config.js
+++ backend/middlewares/multer-config.js
@@ -1,21 +1,39 @@
 const multer = require('multer');
 
 const MIME_TYPES = {
-    'image/jpg': 'jpg',
-    'image/jpeg': 'jpg',
-    'image/png': 'png',
-    'image/gif': 'gif'
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpg',
+  'image/png': 'png',
+  'image/gif': 'gif'
 };
-
+    
 const storage = multer.diskStorage({
-    destination: (req, file, callback) => {
-        callback(null, 'images')
-    },
-    filename: (req, file, callback) => {
-        const name = file.originalname.split('.')[0].split(' ').join('_');
-        const extension = MIME_TYPES[file.mimetype];
-        callback(null, name + '_' + Date.now() + '.' + extension);
-    }
+  destination: (req, file, callback) => {
+    callback(null, 'images')
+  },
+  filename: (req, file, callback) => {
+    const name = file.originalname.split('.')[0].split(' ').join('_');
+    const extension = MIME_TYPES[file.mimetype];
+    callback(null, name + '_' + Date.now() + '.' + extension);
+  }
 })
 
-module.exports = multer({ storage }).single('image');
+const fileFilter = (req, file, callback) => {
+  // Accept images only
+  if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+    return callback(new Error('Seulement les fichiers images sont autorisés!'), false);
+  }
+  callback(null, true);
+};
+
+let upload = multer({ storage, fileFilter }).single('image');
+
+module.exports = (req, res, next) => {
+upload(req, res, (err) => {
+    if (err) {
+      // erreur lors de l'upload (erreur de type de fichier notamment)
+      return res.status(500).json({ error: "Une erreur s'est produite" });
+    }
+    next();
+  });
+}

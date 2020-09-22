a392975b1c6f66512e60c8c72e49afeb33713266
Romain <romain.sire@gmx.com>
2020-09-22 21:28:19 +0200

    Frontend: Intégration de l'ajout d'un post, avec le redimensionnement de l'image

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ImageCroppedEvent } from 'ngx-image-cropper';
 
 import { PublicationsService } from "../../services/publications.service";
+import { AuthService } from "../../services/auth.service";
+import { MessagesService } from "../../services/messages.service";
 
 @Component({
   selector: 'app-home',
@@ -11,16 +14,59 @@ export class HomeComponent implements OnInit {
 
   posts: any;
 
+  imageChangedEvent: any = '';
+  croppedImage: any = '';
+
   constructor(
-    private publicationsService: PublicationsService
+    private publicationsService: PublicationsService,
+    public authService: AuthService,
+    private messagesService: MessagesService
   ) { }
 
   ngOnInit(): void {
+    this.getPosts();
+  }
+
+  /**
+   * Récupérer tous les posts, avec leurs commentaires et leur likes/dislikes
+   */
+  getPosts(): void {
     this.publicationsService.getAllPublications()
       .subscribe(response => {
-        console.log(response.posts);
         this.posts = response.posts;
       })
+  }
+
+  /**
+   * Publication d'un nouveau post
+   */
+  // A - Ajout d'une photo
+  fileChangeEvent(event: any): void {
+    this.imageChangedEvent = event;
+  }
+  imageCropped(event: ImageCroppedEvent) {
+    this.croppedImage = event.base64;
+  }
+  imageLoaded() {
+    document.getElementById('cropper').classList.remove('hidden');
+  }
+  loadImageFailed() {
+    this.messagesService.add(`Erreur lors du chargement de l'image`);
+  }
+  // Transformation de l'image base64 (donnée par "ngx-image-cropper") en fichier exploitable
+  base64ToFile(dataurl, filename) {
+    const arr = dataurl.split(',')
+    const mime = arr[0].match(/:(.*?);/)[1]
+    const bstr = atob(arr[1])
+    let n = bstr.length
+    const u8arr = new Uint8Array(n);
+    while(n--){
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new File([u8arr], filename, {type:mime});
+  }
+  onCroppedImageDone() {
+    document.getElementById('cropper').classList.add('hidden');
   }
 
 }

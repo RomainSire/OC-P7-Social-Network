5ea86099564833bf264612701a86a3e32b0caf41
Romain <romain.sire@gmx.com>
2020-09-24 11:22:28 +0200

    Frontend: Ajout d'une interface pour les posts

---
+++ frontend/src/app/models/Post.ts
@@ -0,0 +1,27 @@
+interface Comment {
+  commentId: number,
+  commentDate: string,
+  commentContent: string,
+  userId: number,
+  userName: string,
+  userPicture: string
+}
+
+interface Likes {
+  LikesNumber: number,
+  DislikesNumber: number,
+  currentUserReaction: number
+}
+
+
+export interface Post {
+  postId: number,
+  postDate: string,
+  postContent: string,
+  postImage: string,
+  userId: number,
+  userName: string,
+  userPicture: string,
+  likes: Likes,
+  comments: [Comment]
+}

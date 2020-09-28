2ba7d36eb296d0c1c268cef0c61b2112840527a5
Romain <romain.sire@gmx.com>
2020-09-28 23:33:56 +0200

    Frontend: Affichage du nombrede notifications dans le header

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -11,7 +11,7 @@ export class HeaderComponent implements OnInit {
 
   constructor(
     public authService: AuthService,
-    private notificationsService: NotificationsService
+    public notificationsService: NotificationsService
   ) { }
 
   ngOnInit(): void {
